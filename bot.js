f76f4aa67deb3cec8d65fdc3accdc49045a298f2
troolerrrrrrr <32039975+troolerrrrrrr@users.noreply.github.com>
2018-07-27 01:01:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,8 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-let rebel;
 client.on("ready", async  => {
-    let guild = client.guilds.get("472156986444873749");
+    let guild = client.guilds.get("408352800133873664");
   let users = guild.members.map(member => member.user.id);
   let i;
   rebel=0;
@@ -19,29 +18,30 @@ if(!check.voiceChannelID){
   rebel++;
 }
 }
-guild.channels.find('id', '472156986444873749').setName(" ONLINE • : "+rebel+"");
+guild.channels.find('id', '469540555886559262').setName(" Mars Online「"+rebel+"」");
   client.setInterval(() =>{
     let d = Date.now()
   }, 5000);
 });
 client.on('voiceStateUpdate', (oldMember, newMember) => {
-    let guild = client.guilds.get("472156986444873749");
+    let guild = client.guilds.get("408352800133873664");
 let newUserChannel = newMember.voiceChannel
 let oldUserChannel = oldMember.voiceChannel
  if(oldUserChannel === undefined && newUserChannel !== undefined) {
    rebel++;
-guild.channels.find('id', '472156986444873749').setName(" ONLINE • : "+rebel+"");
+guild.channels.find('id', '472156986444873749').setName(" Mars Online「"+rebel+"」");
 } else if(newUserChannel === undefined){
   rebel--;
-guild.channels.find('id', '472156986444873749').setName(" ONLINE • : "+rebel+"");
+guild.channels.find('id', '472156986444873749').setName(" Mars Online「"+rebel+"」");
 }
 });
 client.on('message', Codes => {
-  
+
   if(Codes.content === "-صوت") {
-      Codes.channel.send(" المتصلين الان : "+rebel+"");
+      Codes.channel.send(" Voice「"+rebel+"」");
 }
 });
+
 
 
 
